b841b4dc867cdd776bc2e97f9ea377b13e18769d
Prabhav04 <prabhavnarayanan74@gmail.com>
2025-06-28 10:19:56 +0530

    asaad

--- placemnetprep/arrayshifting.py
+++ placemnetprep/arrayshifting.py
@@ -12,15 +12,27 @@ def shift_array_left(arr, k):
     if k == 0:
         return arr  # No shift needed
     return arr[k:] + arr[:k]
-
+# shift a matrix to the right and left by k positions
+def shift_matrix_right(matrix, k):
+    return [shift_array_right(row, k) for row in matrix]
+def shift_matrix_left(matrix, k):
+    return [shift_array_left(row, k) for row in matrix]
 
 if __name__ == '__main__':
-    n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
-    k= int(input().strip())
+
+    arr = [1,2,4,5,6,7,8,9]
+    matrix= [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    k=5
+    # Shift the array to the right and left by k positions
     shifted_array = shift_array_right(arr, k)
     shifted_arrayleft = shift_array_left(arr, k)
+    shifted_matrix = shift_matrix_right(matrix, k)
+    shifted_matrixleft = shift_matrix_left(matrix, k)
     print(" ".join(map(str, shifted_array)))
     print(" ".join(map(str, shifted_arrayleft)))
+    for row in shifted_matrix:
+        print(" ".join(map(str, row)))
+    for row in shifted_matrixleft:
+        print(" ".join(map(str, row)))
 
